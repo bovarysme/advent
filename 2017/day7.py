c3e81cd1c63f523431188349d5096db37d00f569
bovarysme <bovarysme@users.noreply.github.com>
2017-12-08 10:55:51 +0100

    Clean up the solution for day 7

--- 2017/day7.py
+++ 2017/day7.py
@@ -1,75 +1,102 @@
+from collections import defaultdict
+
+
 class Node(object):
-    def __init__(self, name, weight, children):
-        self.name = name
+    def __init__(self, weight, children):
         self.weight = weight
         self.children = children
 
-        self.parent = None
         self.subweight = 0
 
 
-def parse(line):
-    line = line.rstrip()
+def parse(lines):
+    nodes = {}
+    for line in lines:
+        parts = line.split(' -> ')
 
-    parts = line.split(' -> ')
+        name, weight = parts[0].split()
+        weight = int(weight[1:-1])
 
-    name, weight = parts[0].split()
-    weight = int(weight[1:-1])
+        children = set()
+        if len(parts) >= 2:
+            children = set(parts[1].split(', '))
 
-    children = []
-    if len(parts) >= 2:
-        children = parts[1].split(', ')
+        nodes[name] = Node(weight, children)
 
-    return Node(name, weight, children)
+    return nodes
 
 
-def subweight(nodes, name):
-    sw = 0
+def find_root(nodes):
+    roots = set(nodes)
+    for node in nodes.values():
+        roots -= node.children
+
+    assert len(roots) == 1
+
+    return list(roots)[0]
+
+
+def compute_weights(nodes, name):
+    current = nodes[name]
+    current.subweight = sum(compute_weights(nodes, child) for child in current.children)
+
+    return current.weight + current.subweight
+
+
+def find_unbalanced(nodes, name):
+    weights = defaultdict(list)
     for child in nodes[name].children:
-        sw += subweight(nodes, child)
+        weight = nodes[child].weight + nodes[child].subweight
+        weights[weight].append(child)
 
-    nodes[name].subweight = sw
+    if len(weights) == 1:
+        return None
+    else:
+        faulty = [v[0] for v in weights.values() if len(v) == 1][0]
 
-    return nodes[name].weight + sw
+        result = find_unbalanced(nodes, faulty)
+        if result is None:
+            keys = list(weights)
+            diff = abs(keys[0] - keys[1])
+
+            return nodes[faulty].weight - diff
+        else:
+            return result
 
 
 def part_one(nodes):
-    key = list(nodes)[0]
-    node = nodes[key]
-    while node.parent:
-        node = nodes[node.parent]
-
-    return node.name
+    return find_root(nodes)
 
 
-def part_two(nodes, root):
-    subweight(nodes, root)
+def part_two(nodes):
+    root = find_root(nodes)
+    compute_weights(nodes, root)
 
-    # LOOK MOM, NO COMPUTERS!!
-    root = 'cwwwj'  # 'boropxd' <- 'qjvtm' <- root
-
-    current = nodes[root]
-    totals = {}
-    for child in current.children:
-        total = nodes[child].subweight + nodes[child].weight
-        totals[child] = total
-
-    #print(totals)
-
-    return nodes[root].weight - 8
+    return find_unbalanced(nodes, root)
 
 
 if __name__ == '__main__':
-    nodes = {}
+    nodes = parse([
+        'pbga (66)',
+        'xhth (57)',
+        'ebii (61)',
+        'havc (66)',
+        'ktlj (57)',
+        'fwft (72) -> ktlj, cntj, xhth',
+        'qoyq (66)',
+        'padx (45) -> pbga, havc, qoyq',
+        'tknk (41) -> ugml, padx, fwft',
+        'jptl (61)',
+        'ugml (68) -> gyxo, ebii, jptl',
+        'gyxo (61)',
+        'cntj (57)',
+    ])
+
+    assert part_one(nodes) == 'tknk'
+    assert part_two(nodes) == 60
+
     with open('inputs/day7.txt', 'r') as f:
-        for line in f.readlines():
-            node = parse(line)
-            nodes[node.name] = node
+        nodes = parse([line.rstrip() for line in f])
 
-    for node in nodes.values():
-        for child in node.children:
-            nodes[child].parent = node.name
-
-    root = part_one(nodes)
-    print('Answer for part one:', root)
-    print('Answer for part two:', part_two(nodes, root))
+    print('Answer for part one:', part_one(nodes))
+    print('Answer for part two:', part_two(nodes))
